e2f3d529e6f9a73d4ac89dd369c65c9d69e58859
cgm <cgm.cgm@gmail.com>
2017-04-17 17:34:11 +0530

    Style form partial

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ActiveRecord::Base
-	validates :title, presence: true, length: { minimum: 3, maximum: 20 }
+	validates :title, presence: true, length: { minimum: 3, maximum: 80 }
 	validates :description, presence: true, length: { minimum: 10, maximum: 300 }
 	
 end

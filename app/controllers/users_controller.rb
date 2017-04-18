38c5aca2542dd3cecf3116da7b25f247ebba3b16
cgm <cgm.cgm@gmail.com>
2017-04-18 16:55:36 +0530

    test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
 
 	def index
 
-		@users = User.all
+		@users = User.paginate(page: params[:page], per_page: 5)
 
 	end
 
@@ -56,6 +56,7 @@ class UsersController < ApplicationController
 
 			@user = User.find(params[:id])
 
+			@user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
 	end
 
 	private
